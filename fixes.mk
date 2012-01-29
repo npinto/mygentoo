d27b8d2dd9c877b6dcef91c26306f5bf20af0cc3
Nicolas Pinto <pinto@alum.mit.edu>
2012-01-29 02:01:09 -0500

    ENH: add pambase fix

--- fixes.mk
+++ fixes.mk
@@ -1,6 +1,11 @@
-all: libpng14
+all: libpng14-news pambase-freeze
 
-libpng14:
+libpng14-news:
 	revdep-rebuild --library libpng14.so.14 -- --keep-going
 	revdep-rebuild --library libpng14.so.14 -- --keep-going
 	find /usr/ -name '*.la' -exec grep png14 {} + || exit 0
+
+pambase-freeze:
+	emerge '=app-portage/portage-utils-0.3.1'
+	emerge sys-auth/pambase
+	emerge app-portage/portage-utils
