718fa6ada34e7d2726f5df1f00a9dc325cb5fcad
Nicolas Pinto <pinto@alum.mit.edu>
2012-03-11 03:55:05 -0400

    FIX: revdep-rebuild quiet

--- fixes.mk
+++ fixes.mk
@@ -1,8 +1,8 @@
 all: libpng14-news pambase-freeze v8-revdep-rebuild
 
 libpng14-news:
-	revdep-rebuild --library libpng14.so.14 -- --keep-going
-	revdep-rebuild --library libpng14.so.14 -- --keep-going
+	revdep-rebuild --library libpng14.so.14 -- --keep-going -q
+	revdep-rebuild --library libpng14.so.14 -- --keep-going -q
 	find /usr/ -name '*.la' -exec grep png14 {} + || exit 0
 	rm -vf '/usr/lib64/libpng14.so.14'
 
@@ -12,5 +12,5 @@ pambase-freeze:
 	emerge app-portage/portage-utils
 
 v8-revdep-rebuild:
-	revdep-rebuild --library '/usr/lib64/libv8-3.6.6.11.so'
+	revdep-rebuild --library '/usr/lib64/libv8-3.6.6.11.so' -q
 	rm -vf '/usr/lib64/libv8-3.6.6.11.so'
