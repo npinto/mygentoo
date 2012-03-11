724e55929431dc5a64b122b16832c3b287910e22
Nicolas Pinto <pinto@alum.mit.edu>
2012-03-11 04:03:27 -0400

    na

--- fixes.mk
+++ fixes.mk
@@ -1,8 +1,8 @@
 all: libpng14-news pambase-freeze v8-revdep-rebuild
 
 libpng14-news:
-	revdep-rebuild --library libpng14.so.14 -- --keep-going -q
-	revdep-rebuild --library libpng14.so.14 -- --keep-going -q
+	revdep-rebuild -q --library libpng14.so.14 -- --keep-going
+	revdep-rebuild -q --library libpng14.so.14 -- --keep-going
 	find /usr/ -name '*.la' -exec grep png14 {} + || exit 0
 	rm -vf '/usr/lib64/libpng14.so.14'
 
@@ -12,5 +12,5 @@ pambase-freeze:
 	emerge app-portage/portage-utils
 
 v8-revdep-rebuild:
-	revdep-rebuild --library '/usr/lib64/libv8-3.6.6.11.so' -q
+	revdep-rebuild -q --library '/usr/lib64/libv8-3.6.6.11.so'
 	rm -vf '/usr/lib64/libv8-3.6.6.11.so'
