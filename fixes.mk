b8345296c0fe9909bdf6f72af2515674198391d0
Nicolas Pinto <pinto@alum.mit.edu>
2012-03-12 03:53:48 -0400

    FIX: double libpng14

--- fixes.mk
+++ fixes.mk
@@ -2,7 +2,6 @@ all: libpng14-news pambase-freeze v8-revdep-rebuild
 
 libpng14-news:
 	revdep-rebuild -q --library libpng14.so.14 -- --keep-going
-	revdep-rebuild -q --library libpng14.so.14 -- --keep-going
 	find /usr/ -name '*.la' -exec grep png14 {} + || exit 0
 	rm -vf '/usr/lib64/libpng14.so.14'
 
