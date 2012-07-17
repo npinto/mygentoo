b40b65208544602caa59a85d0a4cbbb296315d2b
Nicolas Pinto <pinto@alum.mit.edu>
2012-07-17 14:00:53 -0400

    no comment

--- fixes.mk
+++ fixes.mk
@@ -23,8 +23,8 @@ v8-revdep-rebuild:
 
 glsa-201203-12:
 	emerge --sync --quiet
-	emerge -uN --quiet --oneshot ">=dev-libs/openssl-1.0.0h"
-	CLEAN_DELAY=0 emerge -q -C '<dev-libs/openssl-1.0.0h'
+	emerge -uN --quiet --oneshot ">=dev-libs/openssl-1.0.0g"
+	-CLEAN_DELAY=0 emerge -q -C '<dev-libs/openssl-1.0.0g'
 
  #emerge portage-utils; qlist -I -C x11-drivers/
 
